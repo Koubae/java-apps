2ce5c27037ed6dff00364dae47c05753cf154481
Koubae <58447627+Koubae@users.noreply.github.com>
2023-09-09 18:54:04 +0200

    First Commit

---
+++ app-01-cli/src/main/java/com/koubae/Main_App01Cli.java
@@ -0,0 +1,17 @@
+package com.koubae;
+
+public class Main_App01Cli {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Het");
+        }
+
+        if (1 == 1) {}
+    }
+}
